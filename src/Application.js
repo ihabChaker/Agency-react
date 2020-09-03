ab95b40589fa2323a349830c3ca8464636729539
ihabChaker <hi_benakcha@esi.dz>
2020-09-03 19:39:09 +0200

    commit1

--- src/Application.js
+++ src/Application.js
@@ -1,42 +1,49 @@
 import React from 'react'
-import HighScore from './HighScore'
-import './styles.css'
+import Button from './components/Button'
+import './css/styles.css'
 class Application extends React.Component {
      constructor(props) {
           super(props)
           this.state = {
-               count: 0,
-               overTen: false
+               current: '0',
+               previous: []
           }
      }
-     handleClick = () => {
-          this.setState({
-               count: this.state.count + 1
-          })
-     }
-     resetCount = (e) => {
-          console.log(e)
-          this.setState({
-               count: 0,
-               overTen: false
-          })
-     }
-     componentDidUpdate(props, state) {
-          if (this.state.count > 10 && this.state.count !== state.count && !this.state.overTen) {
-               console.log('overten')
-               this.setState({ overTen: true })
-          }
+     reset = () => { this.setState({ current: '0' }) }
+     addToCurrent = (car) => {
+          console.log(car)
+          this.setState({ current: this.state.current + car })
      }
      render() {
-          let { count } = this.state;
-          return <div>
-               <h1> you clicked {count} times  </h1>
-               <HighScore
-                    overTen={this.state.overTen}
-                    onReset={this.resetCount}
-               />
-               <span><button onClick={() => this.handleClick()}>Click me</button></span>
-          </div>;
+          const buttons = [
+               { symbol: 'C', cols: 3, action: this.reset },
+               { symbol: '/', cols: 1, action: this.addToCurrent },
+               { symbol: '7', cols: 1, action: this.addToCurrent },
+               { symbol: '8', cols: 1, action: this.addToCurrent },
+               { symbol: '9', cols: 1, action: this.addToCurrent },
+               { symbol: '+', cols: 1, action: this.addToCurrent },
+               { symbol: '4', cols: 1, action: this.addToCurrent },
+               { symbol: '5', cols: 1, action: this.addToCurrent },
+               { symbol: '6', cols: 1, action: this.addToCurrent },
+               { symbol: '-', cols: 1, action: this.addToCurrent },
+               { symbol: '1', cols: 1, action: this.addToCurrent },
+               { symbol: '2', cols: 1, action: this.addToCurrent },
+               { symbol: '3', cols: 1, action: this.addToCurrent },
+               { symbol: '*', cols: 1, action: this.addToCurrent },
+               { symbol: '0', cols: 2, action: this.addToCurrent },
+               { symbol: '.', cols: 1, action: this.addToCurrent },
+               { symbol: '=', cols: 1, action: this.addToCurrent },
+          ]
+          return (
+               <div className="Application">
+                    <input className="result" value={this.state.current}></input><br />
+                    {buttons.map((btn, i) => {
+                         return (
+                              <Button key={i} symbol={btn.symbol} col={btn.cols} action={(symbol) => btn.action(symbol)} />
+                         )
+                    })}
+               </div>
+          )
      }
 }
 export default Application
